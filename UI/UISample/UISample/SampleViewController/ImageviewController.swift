70817bedfec660b869bece71e3580e8d798a5c5b
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-19 15:35:35 +0900

    Practice Imageview animation

--- UI/UISample/UISample/SampleViewController/ImageviewController.swift
+++ UI/UISample/UISample/SampleViewController/ImageviewController.swift
@@ -10,8 +10,24 @@ import Foundation
 import UIKit
 
 class ImageviewController: UIViewController{
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let images = (0...9).compactMap{UIImage(systemName: "0\($0).circle")}
+        imageView.animationImages = images
+        imageView.animationDuration = 2
+        imageView.animationRepeatCount = 5
+        
+    }
+    
+    @IBAction func clickStartButton(_ sender: Any) {
+        imageView.startAnimating()
+    }
+    @IBAction func clickStopButton(_ sender: Any) {
+        if imageView.isAnimating {
+            imageView.stopAnimating()
+        }
     }
 }
