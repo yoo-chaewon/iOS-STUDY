247d791eb8d6f647090d01430f683fb5150358f0
유채원 <ohwow5@sookmyung.ac.kr>
2020-02-19 15:20:58 +0900

    Add UISample Project

---
+++ UI/UISample/UISample/SampleViewController/ImageviewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageviewController.swift
+//  UISample
+//
+//  Created by yoo chae won on 2020/02/19.
+//  Copyright © 2020 yoo chae won. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageviewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ UI/UISample/UISample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UISample
+//
+//  Created by yoo chae won on 2020/02/19.
+//  Copyright © 2020 yoo chae won. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let list = ["ImageView", "원숭이"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
