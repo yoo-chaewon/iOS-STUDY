58e074399fa10fe03932f3ba1d7c41223bf39f5e
유채원 <ohwow5@sookmyung.ac.kr>
2020-03-04 01:17:05 +0900

    Study Swift-클로저기본 example
    클로저로 계산 메소드 만들기

--- Swift/Test/Calculator.playground/Contents.swift
+++ Swift/Test/Calculator.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+let add : (Int, Int) -> Int
+add = {(a: Int, b: Int) -> Int in
+    return a + b
+}
+
+let sub : (Int, Int) -> Int
+sub = {(a: Int, b: Int) -> Int in
+    return a-b
+}
+
+let multi: (Int, Int) -> Int
+multi = {(a: Int, b: Int) -> Int in
+    return a * b
+}
+
+let div: (Int, Int) -> Int
+div = {(a: Int, b:Int) -> Int in
+    return a / b
+}
+
+func Calculator(a: Int, b: Int, method: (Int, Int)->Int) -> Int{
+    return method(a, b)
+}
+
+print(Calculator(a: 10, b: 20, method: add))
