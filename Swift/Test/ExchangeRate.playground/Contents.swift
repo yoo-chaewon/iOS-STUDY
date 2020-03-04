8cd1601adde1b9f2ab8a6a50b4b4c0769cc0a033
유채원 <ohwow5@sookmyung.ac.kr>
2020-03-04 17:42:41 +0900

    Test get/set willSet/getSet

--- Swift/Test/ExchangeRate.playground/Contents.swift
+++ Swift/Test/ExchangeRate.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+struct Money{
+    var currentRate: Double = 1100 {
+        willSet{
+            print("환율이 \(currentRate)에서 \(newValue)로 바뀝니다.")
+        }
+        didSet{
+            print("환율이 \(oldValue)에서 \(currentRate)로 변경되었습니다.")
+        }
+    }
+    var dollar: Double = 0
+    var won: Double {
+        get{
+            print("get \(dollar*currentRate)")
+            return dollar * currentRate
+        }
+        set{
+            print("set \(dollar)")
+            dollar = newValue / currentRate
+        }
+    }
+}
 
-var str = "Hello, playground"
+
+var myMomey = Money()
+myMomey.dollar = 10
+print(myMomey.won)
+
+myMomey.currentRate = 1200
+
